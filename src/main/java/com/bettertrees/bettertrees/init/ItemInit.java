807a99ee9b0d47b69f6d63ad7f010be069bc63f5
editspace <editspace90@gmail.com>
2024-06-17 14:54:27 -0700

    removed test tab

--- src/main/java/com/bettertrees/bettertrees/init/ItemInit.java
+++ src/main/java/com/bettertrees/bettertrees/init/ItemInit.java
@@ -29,15 +29,5 @@ public class ItemInit {
         }
     }
 
-    public static class TreeTAB extends CreativeModeTab {
-        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "BetterTreesTab");
-        private TreeTAB(int index, String label) {
-            super(index, label);
-        }
 
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(MARSHMELLOW.get());
-        }
-    }
 }

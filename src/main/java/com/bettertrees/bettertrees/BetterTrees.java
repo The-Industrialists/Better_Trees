208bb54dd2462df241edd5967f57ddb34cd73b4d
ItsTheEditor <editspace90@gmail.com>
2024-06-17 14:01:44 -0700

    added first item

--- src/main/java/com/bettertrees/bettertrees/BetterTrees.java
+++ src/main/java/com/bettertrees/bettertrees/BetterTrees.java
@@ -1,14 +1,13 @@
 package com.bettertrees.bettertrees;
 
-import com.bettertrees.bettertrees.setup.Registration;
-import com.bettertrees.bettertrees.setup.ClientSetup;
-import com.bettertrees.bettertrees.setup.ModSetup;
+
+import com.bettertrees.bettertrees.init.ItemInit;
 import com.mojang.logging.LogUtils;
 
-import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -17,23 +16,21 @@ import org.slf4j.Logger;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(BetterTrees.MODID)
+@Mod(BetterTrees.MOD_ID)
 public class BetterTrees
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    public static final String MODID = "bettertrees";
+    public static final String MOD_ID = "bettertrees";
 
     public BetterTrees()
     {
-        // Register the deferred registry
-        Registration.init();
-        // Register the setup method for modloading
-        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
-        // Register 'ModSetup::init' to be called at mod setup time (server and client)
-        modbus.addListener(ModSetup::init);
-        // Register 'ClientSetup::init' to be called at mod setup time (client only)
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modbus.addListener(ClientSetup::init));
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::setup);
+        ItemInit.ITEMS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
 
---
+++ src/main/java/com/bettertrees/bettertrees/init/ItemInit.java
@@ -0,0 +1,31 @@
+package com.bettertrees.bettertrees.init;
+
+import com.bettertrees.bettertrees.BetterTrees;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BetterTrees.MOD_ID);
+
+    public static final RegistryObject<Item> MARSHMELLOW = ITEMS.register("marshmellow",
+            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+
+
+    public static class ModCreativeTab extends CreativeModeTab {
+        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "BetterTrees");
+        private ModCreativeTab(int index, String label) {
+            super(index, label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(MARSHMELLOW.get());
+        }
+    }
+}
